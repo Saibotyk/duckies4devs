acc88056dc3bdf85ce5d7f653d39b0aad071ce40
Tobias Doboszewicz <tobias13@hotmail.fr>
2023-05-09 12:28:46 +0200

    add pictures functions in mobile and desktop

--- script.js
+++ script.js
@@ -9,13 +9,14 @@ const imgArray = [
     "img/canard-jaune-4-l.png",
     "img/canard-jaune-5-l.png"
 ]
+const imgThumbs = document.querySelector(".pictures-img-js");
 
 let count1 = 0;
 buttonNextMobile.addEventListener('click', function (event) {
-    if (count1 > 3) {
+    if (count1 >= 4) {
         count1 = 0
+        imgThumbs.src = imgArray[count1];
     } else {
-        let imgThumbs = document.querySelector(".pictures-img-js");
         count1++;
         imgThumbs.src = imgArray[count1];
     }
@@ -26,13 +27,24 @@ buttonNextMobile.addEventListener('click', function (event) {
 buttonHandlerMobile.addEventListener('click', function (event) {
     if (count1 <= 0) {
         count1 = 4
+        imgThumbs.src = imgArray[count1];
     } else {
-        let imgThumbs = document.querySelector(".pictures-img-js");
         count1--;
         imgThumbs.src = imgArray[count1];
     }
     console.log(imgThumbs.src);
 })
 
-// PICTURES IN DESKTOP 
+// PICTURES IN DESKTOP
+
+const liThumbs = document.querySelectorAll('.thumbs-js > li > img');;
+liThumbs.forEach(element => {
+    element.addEventListener('mouseover', function(e) {
+        imgThumbs.src = imgArray[element.id];
+    });
+});
+
+// ADD TO BUYLIST
+
+const buttonShop = document.querySelector('.add-cta-js');
 
